e54f9e90fcd50cda07d918884e8b53c2093d0506
Lingyu Cao <jared.ziqxu@gmail.com>
2017-03-05 11:49:24 +0000

    mt model

---
+++ layer_test/model_runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "!!!!!!Model4 4 100"
+(cd hw2_5;./run_exp)
+echo "!!!!!!Model8 8 100"
+(cd hw2_6;./run_exp)
+echo "!!!!!!Model4 4 200"
+(cd hw2_7;./run_exp)
+echo "!!!!!!Model8 8 100"
+(cd hw2_8;./run_exp)
+echo "!!!!!!Model2 2 100"
+(cd hw2_9;./run_exp)
+
---
+++ neuron_test/model_runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#echo "!!!!!!Model2 2 100"
+#cd hw2;./run_exp
+echo "!!!!!!Model2 2 200"
+(cd hw2_2;./run_exp)
+echo "!!!!!!Model2 2 400"
+(cd hw2_3;./run_exp)
+echo "!!!!!!Model2 2 800"
+(cd hw2_4;./run_exp)
+echo "!!!!!!Model4 4 100"
+(cd hw2_5;./run_exp)
+
